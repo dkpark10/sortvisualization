4ae0125229acbaf6e08bf2f2cf3cc6dc347fce42
nowinc10 <dkpark10@naver.com>
2021-03-25 23:34:44 +0900

    2021-03-25

--- app.js
+++ app.js
@@ -4,26 +4,39 @@ const app = express();
 const helmet = require('helmet');
 const compression = require('compression');
 const bodyparser = require('body-parser');
-
-const path = require('path');
-const ejs = require('ejs');
-const fs = require('fs');
-
 const port = 8080;
+
+const session = require('express-session');
+const fileStore = require('session-file-store')(session)
+const secret = require('./secret/session')
+
+const createRainbowColor = require('./lib/rainbowColor');
+const createShuffledList = require('./lib/shuffleList');
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 app.use(compression());
 app.use(helmet());
+app.use(session(secret));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use('/lib', express.static('lib'));
+app.use('/lib', express.static('lib'));       // 다른 곳에 있는 lib 파일 사용하려면
 
 app.get('/', (request, response) => {
-  response.render("index", {});
+
+  if(request.session.sess === undefined){
+    const rainbow = createRainbowColor();
+    const shuffle = createShuffledList(rainbow.length);
+    const sess = {
+      rbw: rainbow,
+      shf: shuffle
+    };
+    request.session.sess = sess;
+  }
+  response.render("index", { e: request.session.sess  });
 });
 
 app.use((request, response, next) => {
