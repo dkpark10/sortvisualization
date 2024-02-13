977c9b7d6f3015be0d944eaf9189b1887e726dc2
dkpark10 <dkpark10@naver.com>
2024-02-14 01:12:34 +0900

    feat: webpack 설정

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { App } from './src/app.js';
+import './index.css';
 
 function main() {
   new App({ target: document.getElementById('root') });
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'index.[hash].js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }),
+    new MiniCssExtractPlugin({
+      filename: 'index.[contenthash:8].css',
+      chunkFilename: 'index.[contenthash:8].chunk.css',
+    }),
+  ],
+};
